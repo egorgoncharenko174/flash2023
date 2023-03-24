8f1a2b28c40991ff541df12e58b166cf89822548
Kseniya Smaliakova <kseniya-smaliakova1993@yandex.ru>
2023-03-24 11:57:42 +0300

    css

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,42 +4,51 @@ const Layout = require('./Layout');
 module.exports = function Home({ topic }) {
   return (
     <Layout>
-      <h1 className="d-flex justify-content-evenly h-100 d-inline-block mt-5">
+      <h1 className="text-center" >
         Выберите тему
       </h1>
 
       <div className="container">
         <div className="card" style={{ width: '25rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="/img/1.jpeg" className="garru-img" alt="..." />
           <div className="card-body">
             <h5 style={{ textAlign: 'center' }} className="card-title">
               {topic[0].title}
             </h5>
-            <a href="http://localhost:4000/topic/1/question/1" className="btn btn-primary">
+            <a
+              href="http://localhost:4000/topic/1/question/1"
+              className="btn btn-primary"
+            >
               Go somewhere
             </a>
           </div>
         </div>
         <div className="card" style={{ width: '25rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="/img/2.jpeg" className="kino-img" alt="..." />
           <div className="card-body">
             <h5 style={{ textAlign: 'center' }} className="card-title">
               {' '}
               {topic[1].title}
             </h5>
-            <a href="http://localhost:4000/topic/2/question/8" className="btn btn-primary">
+            <a
+              href="http://localhost:4000/topic/2/question/8"
+              className="btn btn-primary"
+            >
               Go somewhere
             </a>
           </div>
         </div>
         <div className="card" style={{ width: '25rem' }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="/img/3.jpeg" className="question" alt="..." />
           <div className="card-body">
             <h5 style={{ textAlign: 'center' }} className="card-title">
               {' '}
               {topic[2].title}
             </h5>
-            <a href="http://localhost:4000/topic/3/question/15" className="btn btn-primary">
+            <a
+              href="http://localhost:4000/topic/3/question/15"
+              className="btn btn-primary"
+            >
               Go somewhere
             </a>
           </div>
